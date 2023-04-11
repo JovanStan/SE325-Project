41ed8abadbc65c50cee02ba349ae9131b3e7d935
JovanStan <jovanstankovic561@gmail.com>
2023-04-11 20:32:02 +0200

    loadlevel

--- SE325/Assets/Scripts/Cutscene1.cs
+++ SE325/Assets/Scripts/Cutscene1.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Cutscene1 : MonoBehaviour
 {
@@ -9,6 +10,11 @@ public class Cutscene1 : MonoBehaviour
 	private void Start()
 	{
 		StartCoroutine(ShowButton());
+	}
+
+	public void PlayGame()
+	{
+		SceneManager.LoadScene("TestJovan");
 	}
 
 	IEnumerator ShowButton()
